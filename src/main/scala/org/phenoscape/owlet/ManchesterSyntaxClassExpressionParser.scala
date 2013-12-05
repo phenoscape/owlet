4b031dc2518f5ddcfe22423fb53f9454632e53aa
Jim Balhoff <balhoff@nescent.org>
2013-12-05 11:54:55 -0600

    Reverted regex change after better testing.

--- src/main/scala/org/phenoscape/owlet/ManchesterSyntaxClassExpressionParser.scala
+++ src/main/scala/org/phenoscape/owlet/ManchesterSyntaxClassExpressionParser.scala
@@ -43,10 +43,10 @@ object ManchesterSyntaxClassExpressionParser {
     private val factory = OWLManager.getOWLDataFactory
     // These are modified from http://www.w3.org/TR/2008/REC-rdf-sparql-query-20080115/
     val PN_CHARS_BASE = "[A-Z]|[a-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]|[\u10000-\uEFFFF]"
-    val PN_CHARS_U = """%s|_""".format(PN_CHARS_BASE)
-    val PN_CHARS = """%s|\-|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]""".format(PN_CHARS_U)
-    val PN_PREFIX = """%s((%s|\.)*%s)?""".format(PN_CHARS_BASE, PN_CHARS, PN_CHARS)
-    val PN_LOCAL = """%s|[0-9]((%s|\.)*%s)?""".format(PN_CHARS_U, PN_CHARS, PN_CHARS)
+    val PN_CHARS_U = """(%s)|_""".format(PN_CHARS_BASE)
+    val PN_CHARS = """(%s)|\-|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]""".format(PN_CHARS_U)
+    val PN_PREFIX = """(%s)(((%s)|\.)*(%s))?""".format(PN_CHARS_BASE, PN_CHARS, PN_CHARS)
+    val PN_LOCAL = """((%s)|[0-9])(((%s)|\.)*(%s))?""".format(PN_CHARS_U, PN_CHARS, PN_CHARS)
     val PNAME_NS = """(%s)?:""".format(PN_PREFIX)
     val PNAME_LN = "%s(%s)*".format(PNAME_NS, PN_LOCAL)
 
